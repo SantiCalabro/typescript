6e1a3a127a46949776b9d4064dcca6f31cb571f8
SantiCalabro <santiagocalabro.dev@gmail.com>
2025-11-20 18:00:22 -0300

    notes}

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,4 @@ const server = express();
 server.use(express.json())
 server.use(router)
 
-export default server; //Sirve para cuando quiero exportar una sola cosa.
+export default server; //Export Default sirve para cuando quiero exportar una sola cosa.
